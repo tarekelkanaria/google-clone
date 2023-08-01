2d3fcc7c6d50bb61dc3385f0e2ef0d22d0e90e4f
Tarek Elkanaria <tarekelkanaria@gmail.com>
2023-08-01 12:13:20 +0300

    Add country API

--- src/components/MainFooter/UserCountry/index.tsx
+++ src/components/MainFooter/UserCountry/index.tsx
@@ -4,44 +4,32 @@ import { useEffect, useState } from "react";
 import ClipLoader from "react-spinners/ClipLoader";
 import useSWR from "swr";
 
-const USER_NAME = process.env.NEXT_PUBLIC_USER_NAME;
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
-const fetcher = async (url: string) => {
-  const response = await fetch(url)
+const fetcher = async () => {
+  const response = await fetch(
+    `https://extreme-ip-lookup.com/json/?key=${API_KEY}`
+  )
     .then((res) => res.json())
-    .then((data) => data.countryName);
+    .then((data) => data.country);
 
   return response;
 };
 
 const UserCountry = () => {
-  const [mounted, setMounted] = useState(false);
-  const [lat, setLat] = useState(37.09024);
-  const [long, setLong] = useState(-95.712891);
-  const { data, error, isLoading } = useSWR(
-    mounted ? "user-country" : null,
-    () =>
-      fetcher(
-        `https://api.geonames.org/countryCodeJSON?lat=${lat}&lng=${long}&username=${USER_NAME}`
-      )
-  );
+  const [country, setCountry] = useState("United States");
+  const { data, error, isLoading } = useSWR("user-country", fetcher);
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords;
-        setLat(latitude);
-        setLong(longitude);
-      });
-    }
-    setMounted(true);
-  }, []);
+    setCountry(data);
+  }, [data]);
+
   return (
     <>
       <p className="px-8 py-3 border-b border-[#dadce0]">
-        <ClipLoader color="#93c5fd" loading={isLoading} size={30} />
-        {error && <p>Country Not Found</p>}
-        {data}
+        <ClipLoader color="#93c5fd" loading={isLoading} size={20} />
+        {error && "Country Not Found"}
+        {data && country}
       </p>
     </>
   );

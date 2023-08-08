d9e2eb440fcaaf32d72965f08e1cd9bf5689c2f7
Tarek Elkanaria <tarekelkanaria@gmail.com>
2023-08-08 11:44:04 +0300

    Remove speech debug and next icon

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -5,11 +5,7 @@ import { SpeechProvider } from "@speechly/react-client";
 const appId = process.env.NEXT_PUBLIC_SPEECHLY_APP_ID;
 
 const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return (
-    <SpeechProvider appId={appId} debug logSegments>
-      {children}
-    </SpeechProvider>
-  );
+  return <SpeechProvider appId={appId}>{children}</SpeechProvider>;
 };
 
 export default Providers;
